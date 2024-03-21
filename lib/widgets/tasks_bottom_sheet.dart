df5efb6ca43a85ac1fcd87a84b647729c7a267ad
Mohamed <mohamed.osama200538@gmail.com>
2024-03-21 06:07:20 +0200

    Handeled Validation layer on TaskFormField

--- lib/widgets/tasks_bottom_sheet.dart
+++ lib/widgets/tasks_bottom_sheet.dart
@@ -5,6 +5,8 @@ class AddTaskBottomSheet extends StatelessWidget {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
 
+  var formKey = GlobalKey<FormState>();
+
   AddTaskBottomSheet({super.key});
 
   @override
@@ -19,62 +21,81 @@ class AddTaskBottomSheet extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
           color: Theme.of(context).bottomSheetTheme.backgroundColor,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Add New Task",
-              style: TextStyle(
-                color: Theme.of(context).textTheme.bodyMedium!.color,
-                fontSize: 18,
-                fontWeight: FontWeight.w700,
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Add New Task",
+                style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyMedium!.color,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 28),
-            CustomTextFormField(
-              labelText: "Enter your task title",
-              controller: titleController,
-              textInputType: TextInputType.text,
-              validator: (text) {},
-            ),
-            const SizedBox(height: 12),
-            CustomTextFormField(
-              labelText: "Add your task description",
-              controller: descriptionController,
-              textInputType: TextInputType.text,
-              validator: (text) {},
-              maxLines: 20,
-            ),
-            const SizedBox(height: 26),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Select Time",
-                  style: TextStyle(
-                    color: Theme.of(context).unselectedWidgetColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+              const SizedBox(height: 28),
+              CustomTextFormField(
+                labelText: "Enter your task title",
+                controller: titleController,
+                textInputType: TextInputType.text,
+                validator: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return 'Please enter task title';
+                  }
+                },
+              ),
+              const SizedBox(height: 12),
+              CustomTextFormField(
+                labelText: "Add your task description",
+                controller: descriptionController,
+                textInputType: TextInputType.text,
+                maxLines: 20,
+                validator: (text) {
+                  if (text == null || text.trim().isEmpty) {
+                    return 'Task must have a description';
+                  }
+                },
+              ),
+              const SizedBox(height: 26),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Select Time",
+                    style: TextStyle(
+                      color: Theme.of(context).unselectedWidgetColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 64),
+              ElevatedButton(
+                onPressed: () {
+                  addTask();
+                },
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 64),
-            ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-              ),
-              child: const Text("Add Task"),
-            )
-          ],
+                child: const Text("Add Task"),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void addTask() {
+    if (formKey.currentState!.validate() == false) {
+      return;
+    }
   }
 }
